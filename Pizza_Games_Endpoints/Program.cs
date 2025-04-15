686c0e54bb398f2e26ac001556d2fb8128f3fc96
bearismybrother <thetwai123456789@gmail.com>
2025-04-14 22:10:16 -0700

    Change to using env variable of user secret for deploying Render

--- Pizza_Games_Endpoints/Program.cs
+++ Pizza_Games_Endpoints/Program.cs
@@ -5,8 +5,14 @@ using Pizza_Games_Endpoints.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Using user secrets
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseNpgsql(builder.Configuration.GetConnectionString("Remote"))
+//);
+
+// Using env variables
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("Remote"))
+    options.UseNpgsql(Environment.GetEnvironmentVariable("ConnectionString"))
 );
 
 var app = builder.Build();
