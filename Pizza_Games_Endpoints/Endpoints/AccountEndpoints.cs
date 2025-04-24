ac49dd45f2aaa05c95fdbf79a34b5afbe57b17a4
bearismybrother <thetwai123456789@gmail.com>
2025-04-24 12:34:46 -0700

    Added an endpoint to validate token manually

--- Pizza_Games_Endpoints/Endpoints/AccountEndpoints.cs
+++ Pizza_Games_Endpoints/Endpoints/AccountEndpoints.cs
@@ -1,9 +1,14 @@
 ﻿using System.Data.Common;
+using System.IdentityModel.Tokens.Jwt;
 using System.Runtime.CompilerServices;
+using System.Security.Claims;
+using System.Text;
 using EntityFramework.Exceptions.Common;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Pizza_Games_Endpoints.AuthenticationMethods;
 using Pizza_Games_Endpoints.Models;
 
@@ -15,6 +20,7 @@ namespace Pizza_Games_Endpoints.Endpoints
         {
             group.MapPost("/login", AuthenticateAccount);
             group.MapPost("/register", CreateAccount);
+            group.MapPost("/validateToken", ValidateToken);
             return group;
         }
 
@@ -63,6 +69,24 @@ namespace Pizza_Games_Endpoints.Endpoints
             {
                 return TypedResults.BadRequest("Something went wrong with logging in");
             }
+        }
+
+        [Authorize]
+        public static async Task<IResult> ValidateToken()
+        {
+            //var handler = new JwtSecurityTokenHandler();
+            //var jwtToken = handler.ReadJwtToken(token);
+            //var expInSeconds = jwtToken.Payload.Expiration;
+            //var expDate = DateTimeOffset.FromUnixTimeSeconds((long)expInSeconds);
+            //if (expDate > DateTime.UtcNow)
+            //{
+            //    return TypedResults.Ok();
+            //}
+            //else
+            //{
+            //    return TypedResults.BadRequest();
+            //}
+            return TypedResults.Ok();
         }
     }
 }
